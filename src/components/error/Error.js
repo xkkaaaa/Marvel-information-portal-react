bcb6a86a32e865eec1fe1f2f81ec125cce312340
xkkaaaa <cholponbaevb655@gmail.com>
2022-04-15 21:32:10 +0600

    random chair, error, class

---
+++ src/components/error/Error.js
@@ -0,0 +1,19 @@
+import error from './error.gif'
+
+const Error = () => {
+  return (
+    <img
+      src={error}
+      alt="error"
+      style={{
+        display: 'block',
+        width: '250px',
+        height: '250px',
+        objectFit: 'contain',
+        margin: '0 auto',
+      }}
+    />
+  )
+}
+
+export default Error
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,19 +3,37 @@ class MarvelService {
   _apiKey = 'apikey=400c99960a81a06c505cc34cbcdf002e'
 
   getResource = async (url) => {
-    let response = await fetch(url)
-
-    if (!response.ok) {
-      throw new Error(`Could not fetch ${url}, status: ${response.status}`)
+    let res = await fetch(url)
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`)
     }
-    return await response.json()
+    return await res.json()
   }
 
-  getAllCharacters = () => {
-    return this.getResource(`${this._apiBase}characters?limit=9&offset=215&${this._apiKey}`)
+  getAllCharacters = async () => {
+    const res = await this.getResource(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    )
+    return res.data.results.map(this._transformCharacter)
   }
-  getCharacter = (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+
+  getCharacter = async (id) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    )
+    return this._transformCharacter(res.data.results[0])
+  }
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description
+        ? `${char.description.slice(0, 210)}...`
+        : 'There is no description for this character',
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    }
   }
 }
 
