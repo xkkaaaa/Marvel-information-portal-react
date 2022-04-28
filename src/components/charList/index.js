f2abe1498e3407fa20bd0ee5afb766dcf32ea699
xkkaaaa <cholponbaevb655@gmail.com>
2022-04-28 16:43:42 +0600

    fixed bug, last commit

--- src/components/charList/index.js
+++ src/components/charList/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import useMarvelService from '../../services/MarvelService'
 import Spinner from '../spinner'
@@ -91,9 +91,13 @@ const CharList = (props) => {
     return <ul className="char__grid">{items}</ul>
   }
 
+    const element = useMemo(() => {
+      return setContent(process, () => renderItems(charList), newItemLoading)
+    }, [process])
+
   return (
     <div className="char__list">
-      {setContent(process, () => renderItems(charList), newItemLoading)}
+      {element}
       <button
         className="button button__main button__long"
         disabled={newItemLoading}
