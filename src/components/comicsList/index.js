6154d288ea2a3faeb4a22b49923716cb429a537a
xkkaaaa <cholponbaevb655@gmail.com>
2022-06-29 00:16:58 +0600

    fixed comicsList

--- src/components/comicsList/index.js
+++ src/components/comicsList/index.js
@@ -56,10 +56,9 @@ const ComicsList = () => {
       return (
         <li className="comics__item" key={i}>
           <Link to={`/comics/${item.id}`}>
-            <img
-              src={item.thumbnail}
-              alt={item.title}
-              className="comics__item-img"/>
+            <div className="comics__item-img">
+              <img src={item.thumbnail} alt={item.title} />
+            </div>
             <div className="comics__item-name">{item.title}</div>
             <div className="comics__item-price">{item.price}</div>
           </Link>
